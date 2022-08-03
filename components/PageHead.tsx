7b54e8ed84a0ee64a122e58f152acbd26c41d7a2
Francisco Moretti <franciscoemoretti@gmail.com>
2022-08-03 13:53:32 +0100

    add width and height to social image

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -56,6 +56,8 @@ export const PageHead: React.FC<
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:image' content={socialImageUrl} />
           <meta property='og:image' content={socialImageUrl} />
+          <meta property="og:image:width" content="1600"/>
+          <meta property="og:image:height" content="836"/>
         </>
       ) : (
         <meta name='twitter:card' content='summary' />
