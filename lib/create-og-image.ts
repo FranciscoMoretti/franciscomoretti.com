a8cbc95a56ad6ec36b94ba686958725ea87959a7
Francisco Moretti <franciscoemoretti@gmail.com>
2022-08-12 09:46:23 +0100

    OgImage updates

--- lib/create-og-image.ts
+++ lib/create-og-image.ts
@@ -13,26 +13,26 @@ export const createOgImage = ({
    
       // TITLE
       // Karla google font in light rose
-      `l_text:Karla_54_bold:${e(title)},co_rgb:080C0C,c_fit,w_1000,h_240`,
+      `l_text:Karla_54_bold:${e(title)},co_rgb:0C0C0C,c_fit,w_1000,h_140`,
       // Positioning
-      `fl_layer_apply,g_south_west,x_100,y_200`,
+      `fl_layer_apply,g_north_west,x_100,y_100`,
    
       // META
       // Karla, but smaller
-      `l_text:Karla_36:${e(meta)},co_rgb:080C0C90,c_fit,w_1000`,
+      `l_text:Karla_36:${e(meta)},co_rgb:0C0C0CB0,c_fit,w_1000`,
       // Positioning
-      `fl_layer_apply,g_south_west,x_100,y_100`,
+      `fl_layer_apply,g_north_west,x_100,y_250`,
    
       // PROFILE IMAGE
       // dynamically fetched from my twitter profile
       `l_twitter_name:franmoretti_`,
-      // Transformations
+      // Transformatio``ns
       `c_thumb,g_face,r_max,w_380,h_380,q_100`,
       // Positioning
-      `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
+      `fl_layer_apply,w_160,g_south_west,x_100,y_100`,
    
       // BG
-      `shapelined.png`,
+      `lego_social_image_background.png`,
    
   ].join("/")
    
