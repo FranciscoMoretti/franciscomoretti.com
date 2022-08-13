e4fe8a4c53ae83950cd20d76eda8aad591f2a375
Francisco Moretti <franciscoemoretti@gmail.com>
2022-08-13 18:26:52 +0100

    add custom page cover position

--- site.config.ts
+++ site.config.ts
@@ -27,7 +27,7 @@ export default siteConfig({
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: 'https://images.unsplash.com/photo-1586776977607-310e9c725c37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80',
-  defaultPageCoverPosition: 0.5,
+  defaultPageCoverPosition: 0.75,
 
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
