ffe46bc594e7e1f0790f60b12fc3010549bf25e0
Francisco Moretti <franciscoemoretti@gmail.com>
2023-03-11 08:24:52 +0000

    removed images from orginal repo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,21 +11,9 @@ export default class MyDocument extends Document {
           <Head>
             <link rel='shortcut icon' href='/favicon.ico' />
             <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon-32x32.png'
-            />
-            <link
               rel='apple-touch-icon'
               sizes='180x180'
               href='/apple-touch-icon.png'
-            />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='96x96'
-              href='/favicon-96x96.png'
             />
             <link rel='manifest' href='/site.webmanifest' />
           </Head>
